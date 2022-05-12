41d5ece81c9514e8d5775bdb0d5b12a56a263a87
Trhod17 <hedgehog.thomas@gmail.com>
2022-05-12 10:01:05 +1000

    commit

--- plantronics/urls.py
+++ plantronics/urls.py
@@ -23,8 +23,7 @@ router.register(r'edible', views.EdibleViewSet, basename='edible')
 # And for baton which controls the styling for the admin interface
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls',
-                              namespace='rest_framework')),
+    path('api-auth/', include('rest_framework.urls')),
     path('backend/', admin.site.urls),
     path('baton/', include('baton.urls')),
     path('login/', views.login),
